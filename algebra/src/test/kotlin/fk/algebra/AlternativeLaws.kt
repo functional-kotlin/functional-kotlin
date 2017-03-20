48e413d132136aac85e6ac9c85c35eeb522c1f48
Michael Pardo <kensuke155@gmail.com>
2017-03-20 13:31:27 -0400

    Add Alternative laws.

--- algebra/src/test/kotlin/fk/algebra/AlternativeLaws.kt
+++ algebra/src/test/kotlin/fk/algebra/AlternativeLaws.kt
@@ -1,7 +1,38 @@
 package fk.algebra
 
+import com.pholser.junit.quickcheck.Property
+import com.pholser.junit.quickcheck.When
+import fk.Properties.NOT_NULL
+
 interface AlternativeLaws : PlusLaws, ApplicativeLaws {
 
     override fun <A : Any> of(): (a: A) -> Alternative<A>
+
+    override fun <A : Any> zero(): () -> Alternative<A>
+
+    @Property
+    fun <A : Any> alternativeDistributivity(
+            @When(satisfies = NOT_NULL) a: A) {
+
+        val of = of<A>()
+        val x = of(a)
+        val f = of(a)
+
+//        assert(
+//                x.ap(f.alt(g)) == x.ap(f).alt(x.ap(g))
+//        )
+    }
+
+    @Property
+    fun <A : Any> alternativeAnnihilation(
+            @When(satisfies = NOT_NULL) a: A) {
+
+        val of = of<A>()
+        val zero = zero<(A) -> A>()
+
+        assert(
+                of(a).ap(zero()) == zero()
+        )
+    }
 
 }
