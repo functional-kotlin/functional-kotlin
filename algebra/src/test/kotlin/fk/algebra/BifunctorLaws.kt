18756374b24f441473d3977d6f309ec1c83f640b
Michael Pardo <kensuke155@gmail.com>
2017-03-20 13:31:01 -0400

    Add Bifunctor laws.

--- algebra/src/test/kotlin/fk/algebra/BifunctorLaws.kt
+++ algebra/src/test/kotlin/fk/algebra/BifunctorLaws.kt
@@ -1,5 +1,41 @@
 package fk.algebra
 
+import com.pholser.junit.quickcheck.Property
+import com.pholser.junit.quickcheck.When
+import fk.Properties.NOT_NULL
+import fk.compose
+import fk.identity
+
 interface BifunctorLaws {
+
+    fun <A : Any, B : Any> of(): (a: A) -> Bifunctor<A, B>
+
+    @Property
+    fun <A : Any, B : Any> bifunctorIdentity(
+            @When(satisfies = NOT_NULL) a: A) {
+
+        val of = of<A, B>()
+
+        val x = of(a).bimap(identity<A>(), identity<B>())
+        val y = of(a)
+
+        assert(
+                x == y
+        )
+    }
+
+    @Property
+    fun <A : Any, B : Any> bifunctorComposition(
+            @When(satisfies = NOT_NULL) a: A) {
+
+        val of = of<A, B>()
+
+        val x = of(a).bimap(identity<A>() compose identity<A>(), identity<B>() compose identity<B>())
+        val y = of(a).bimap(identity<A>(), identity<B>()).bimap(identity<A>(), identity<B>())
+
+        assert(
+                x == y
+        )
+    }
 
 }
